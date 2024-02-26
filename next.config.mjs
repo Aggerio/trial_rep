926fe77f78b6e9f72f3c95d9ed35cd7936460a09
Aggerio <agamsrivastava004@gmail.com>
2024-02-26 23:31:32 +0530

    Export

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: "/trial_rep",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
